36bd93d88d54ba8268e8b76fd54b0182cb3a20fa
Lemon <lem0nbr0.627@gmail.com>
2021-10-05 23:47:38 +0500

    redesign

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
     <div class="wrapper">
-      <div class="wrapper_video">
-        <div class="wrapper_video__video">
+      <div class="wrapper_top">
+        <div class="wrapper_top_date">{{ ClockDate }}</div>
+        <div class="wrapper_top_time">{{ ClockTime }}</div>
+        <div class="wrapper_top_temp">{{ weatherTemp }}&#176;C</div>
+      </div>
+      <div class="wrapper_bottom">
+        <div class="wrapper_bottom_video">
           <video ref="video" autoplay preload @ended="endedPlaying"/>
         </div>
-        <div class="spacer"/>
-        <div class="wrapper_video__runningstring">
-          <marquee direction="left" scrollamount="20">{{ runningString }}</marquee>
-        </div>
-        <div class="spacer"/>
-      </div>
-      <div class="wrapper_content">
-        <div class="wrapper_content__time">
-          <h1 class="block_time">{{ ClockTime }}</h1>
-          <!--          <img alt="" class="medved" src="../public/medved.png"/>-->
-          <h2 class="block_date">{{ ClockDate }}</h2>
-        </div>
-        <hr class="hr__time_weather" />
-        <div class="wrapper_content__weather">
-          <div class="weather_title">Сейчас за окном</div>
-          <ul>
-            <li><div class="weather_cloud">Облачность: {{ weatherCloud }}</div></li>
-            <li><div class="weather_temp">Температура: {{ weatherTemp }}&#176;</div></li>
-            <li><div class="weather_fells">Ощущается как: {{ weatherFeel }}&#176;</div></li>
-            <li><div class="weather_humidity">Влажность: {{ weatherHumidity }}%</div></li>
-            <li><div class="weather_wind">Скорость ветра: {{ weatherWind }} км/ч</div></li>
-          </ul>
+        <div class="wrapper_bottom_sidebar">
+          <div class="wrapper_bottom_sidebar_news_title">Новость дня:</div>
+          <div class="wrapper_bottom_sidebar_news">С 1 октября в университете появится Яндекс.Такси</div>
+          <div class="wrapper_bottom_sidebar_logo">
+            <img src="../public/company.png" alt="" class="company_logo"/>
+          </div>
         </div>
       </div>
     </div>
@@ -44,14 +33,7 @@ export default {
       time_timer: '',
 
       weatherTemp: null,
-      weatherFeel: null,
-      weatherCloud: '',
-      weatherWind: null,
-      weatherHumidity: null,
       weather_timer: '',
-
-      runningString: '',
-      string_timer: ''
     }
   },
   methods: {
@@ -66,30 +48,16 @@ export default {
           });
       // console.log(this.urlVideo)
     },
-    getRunningString() {
-      let axios = require('axios');
-      axios.get('http://127.0.0.1:8000/getRunningString')
-          .then((response) => {
-            // handle success
-            console.log(response.data);
-            this.runningString = response.data.string
-          });
-      // console.log(this.urlVideo)
-    },
     getWeather() {
       let axios = require('axios');
-      axios.get('https://api.openweathermap.org/data/2.5/weather?lat=58.006817&lon=56.1860062&lang=ru&units=metric&appid=737f5257f034b715433c5446195947b4')
+      axios.get('https://api.openweathermap.org/data/2.5/weather?lat=58.006817&lon=56.1860062&lang=ru&units=metric&appid=')
           .then((response) => {
-            console.log(response);
+            // console.log(response);
             this.weatherTemp = response.data.main.temp;
-            this.weatherFeel = response.data.main.feels_like;
-            this.weatherWind = response.data.wind.speed;
-            this.weatherCloud = response.data.weather[0].description;
-            this.weatherHumidity = response.data.main.humidity;
           });
     },
     getNow: function() {
-      [this.ClockDate, this.ClockTime] = ((l,d) => [d.toLocaleDateString(l).replace(/\./g, '-'), d.toLocaleTimeString(l).slice(0,-3)])('ru-RU', new Date());    },
+      [this.ClockDate, this.ClockTime] = ((l,d) => [d.toLocaleDateString(l).slice(0,-5), d.toLocaleTimeString(l).slice(0,-3)])('ru-RU', new Date());    },
     endedPlaying(){
       // console.log("ended");
       this.getVideo();
@@ -98,17 +66,23 @@ export default {
   mounted() {
     this.getVideo()
     this.getWeather()
-    this.getRunningString()
-    this.string_timer = setInterval(this.getRunningString, 60000)
-    this.weather_timer = setInterval(this.getWeather, 60000)
+    this.weather_timer = setInterval(this.getWeather, 3600000)
     this.time_timer = setInterval(this.getNow, 1000)
   }
 }
 </script>
 <style>
 @font-face {
-  font-family: 'PermianSlabSerif-Typeface';
-  src: local("/fonts/PermianSlabSerifTypeface.woff2");
+  font-family: 'HalvarBreit-Md';
+  src: url("../public/fonts/HalvarBreit-Md.ttf") format('truetype');
+}
+@font-face {
+  font-family: 'HalvarBreit-Th';
+  src: url("../public/fonts/HalvarBreit-Th.ttf") format('truetype');
+}
+@font-face {
+  font-family: 'PermianSansTypeface';
+  src: url("../public/fonts/PermianSansTypeface.ttf") format('truetype');
 }
 #app{
   height: 100vh;
@@ -117,81 +91,94 @@ export default {
   margin: 0;
   padding: 0;
 }
-.hr__time_weather{
-  margin-top: 50px;
-  margin-bottom: 50px;
-  width: 280px;
-  margin-left: 50px;
+.wrapper_top{
+  display: flex;
+  flex-direction: row;
 }
-.wrapper {
-  /*background-color: #c62e3e;*/
-  background-color: #888;
-  color: white;
-  /*height: 150px;*/
-  padding: 18px;
+.wrapper_top_date{
+  width: 500px;
+  height: 139px;
+  top: 81px;
+  left: 50px;
+  font-family: HalvarBreit-Th,sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 120px;
+  line-height: 139px;
+  display: flex;
+  color: #C62E3E;
+}
+.wrapper_top_time{
+  width: 620px;
+  height: 180px;
+  left: 649px;
+  top: 63px;
+  padding-left: 100px;
+  padding-right: 100px;
+  font-family: HalvarBreit-Md,sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 140px;
+  line-height: 162px;
+  text-align: center;
+  color: #C62E3E;
+}
+.wrapper_top_temp{
+  width: 500px;
+  height: 139px;
+  left: 1380px;
+  top: 81px;
+  font-family: HalvarBreit-Th,sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 120px;
+  line-height: 139px;
+  text-align: right;
+  color: #C62E3E;
+}
+.wrapper_bottom_video {
+  width: 1280px;
+  height: 720px;
+  padding-top: 80px;
+}
+.wrapper_bottom{
   flex-direction: row;
   display: flex;
-  height: calc(100% - 36px);
 }
-
-.wrapper_video {
-  background-color: #c62e3e;
-  /*background-color: #888;*/
-  /*height: 150px;*/
-  flex: 1 1 1500px;
-  display: flex;
-  flex-direction: column;
+.wrapper_bottom_sidebar_news_title{
+  padding-left: 100px;
+  font-family: PermianSansTypeface,sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 50px;
+  line-height: 63px;
+  color: #C62E3E;
+  padding-top: 80px;
 }
-.wrapper_content {
-  background-color: #888;
-  /*background-color: #c62e3e;*/
-  /*height: 150px;*/
-  flex: 1 1 420px;
+.wrapper_bottom_sidebar_news{
+  padding-left: 100px;
+  padding-top: 40px;
+  font-family: PermianSansTypeface, sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 50px;
+  line-height: 63px;
+  color: #C62E3E;
+  height: 450px;
 }
-video {
-  /*height: 0px;*/
-  width: 1500px;
+.wrapper_bottom_sidebar_logo{
+  padding-left: 100px;
+  padding-top: 50px;
+  width: 440px;
 }
-.spacer {
-  flex: 1 1 100%;
-  display: block;
-  background-color: #c62e3e;
-  /*background-color: #888;*/
+.company_logo{
+  width: 440px;
 }
-.wrapper_video__runningstring{
-  /*background-color: #b51d0d;*/
-  background-color: #c62e3e;
-  margin-top: 5px;
-  width: 1500px;
-  padding-bottom: 20px;
-  font-size: 3.2em;
-  font-family: PermianSlabSerif-Typeface,serif;
-}
-img {
-  margin-right: 20px;
-}
-.wrapper_content__weather {
-  padding-left:19%;
-  /*padding-right: 12%;*/
-  font-size: 1.8em;
-}
-.weather_title {
-  margin-left: -28px;
-  font-size: 1.4em;
-}
-.wrapper_content__time{
-  padding-top: 10%;
-  margin-left: 50px;
+.wrapper {
+  background-color: #fff;
   color: white;
-}
-.block_time{
-  text-align: justify;
-  /*margin-left: 13%;*/
-  font-size: 7.4em;
-}
-.block_date{
-  /*margin-top: 5%;*/
-  /*margin-left: 30%;*/
-  font-size: 3.8em;
+  padding: 50px;
+  flex-direction: column;
+  display: flex;
 }
 </style>
